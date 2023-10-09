b910d9820914a920b66522016e209669c510f3cd
herawatiajis <99889012+herawatiajis@users.noreply.github.com>
2023-10-09 06:58:30 -0700

    Add files via upload

---
+++ day1.java
@@ -0,0 +1,13 @@
+package isc;
+public class day1 {
+    public static void main(String[] args) {
+        // Hari pertama dari 100 hari
+        System.out.println("Hello World");
+        System.out.println(2023);
+        
+        /* ini adalah cara untuk membuat komentar yang lebih dari satu baris.
+        dihari pertama dari 100 hari ini kita akan belajar membuat komentar dan mencetak/menampilkan data.
+        */
+    }
+    
+}

9b67b233b11c80f4f0aeb4a88c0553292f4a61c6
herawatiajis <99889012+herawatiajis@users.noreply.github.com>
2023-10-22 21:19:26 +0800

    perulangan (while)

---
+++ day14.java
@@ -0,0 +1,12 @@
+package isc;
+public class day14 {
+    public static void main(String[] args) {
+        // Hari ke empatbelas dari 100 hari
+        int number = 2;
+        while (number <= 10) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+    
+}

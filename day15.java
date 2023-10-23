7c9c6a54f1fa400f7d959fdb47a87d017513e022
herawatiajis <99889012+herawatiajis@users.noreply.github.com>
2023-10-23 07:07:08 -0700

    perulangan do while

---
+++ day15.java
@@ -0,0 +1,13 @@
+package isc;
+public class day15 {
+    public static void main(String[] args) {
+        // Hari ke 15 dari 100 hari
+        int i = 1;
+        
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+    
+}

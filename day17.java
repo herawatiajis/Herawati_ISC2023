0e44be81b1583da58741672ee822229ab28a75e7
herawatiajis <99889012+herawatiajis@users.noreply.github.com>
2023-10-25 07:49:51 -0700

    perulangan for

---
+++ day17.java
@@ -0,0 +1,10 @@
+package isc;
+public class day17 {
+    public static void main(String[] args) {
+        //Hari ke tujuhbelas dari 100 hari
+        for (int angka = 1; angka <= 5; angka++) {
+            System.out.println("angka ke-"+ angka);
+        }
+    }
+    
+}

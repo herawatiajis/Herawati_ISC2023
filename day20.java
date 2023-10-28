bdf8b895008d849d2d08f7dfe258440b0bdbf970
herawatiajis <99889012+herawatiajis@users.noreply.github.com>
2023-10-28 06:03:38 -0700

    Array : part 3 (ascending sorting)

---
+++ day20.java
@@ -0,0 +1,16 @@
+package isc;
+import java.util.Arrays;
+public class day20 {
+
+    public static void main(String[] args) {
+        // Hari ke duapuluh dari 100 hari
+        int[] angkaBulat = {5, 2, 9, 1, 5, 6};
+
+        // mengurutkan item array secara ascending
+        Arrays.sort(angkaBulat);
+
+        // Menampilkan array yang telah di sorting
+        System.out.println(Arrays.toString(angkaBulat));
+    }
+    
+}

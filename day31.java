f4d828d2ff0f6befe0266789d3018d47d3983d4b
herawatiajis <99889012+herawatiajis@users.noreply.github.com>
2023-11-08 20:10:53 +0800

    mencetak string dengan infinite loop for

---
+++ day31.java
@@ -0,0 +1,10 @@
+package isc;
+public class day31 {
+    public static void main(String[] args) {
+        // Hari ke tigapuluh satu dari 100 hari
+        for (;;) {
+            System.out.println("Saya tidak terbatas");
+        }
+    }
+    
+}

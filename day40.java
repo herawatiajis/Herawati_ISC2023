73f9db876219cc56672372a59d0406fa6822665f
herawatiajis <99889012+herawatiajis@users.noreply.github.com>
2023-11-17 19:49:16 +0800

    menampilkan isi array menggunakan perulangan dan berdasarkan index

---
+++ day40.java
@@ -0,0 +1,18 @@
+package isc;
+public class day40 {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+        // Menampilkan nilai array menggunakan perulangan
+        System.out.print("Nilai array : ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+    
+}
